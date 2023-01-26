dde443c4e57dcbf0fb1f48a3eae20389ac7b20ed
LEESEUNGYEOL <87690279+tigermint@users.noreply.github.com>
2023-01-26 22:04:21 +0900

    [Silver I] Title: 1로 만들기 2, Time: 72 ms, Memory: 41132 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -1,22 +1,20 @@
-N = int(input())
+from collections import deque
 
-result = [[0, []] for _ in range(N + 1)] 
-result[1][0] = 0
-result[1][1] = [1]
+n = int(input())
+q = deque()
+q.append((n,[n]))
+visited = [0]*(n+1)
 
-for i in range(2, N + 1):
-	
-    result[i][0] = result[i-1][0] + 1
-    result[i][1] = result[i-1][1] + [i]
-    
-    if i % 3 == 0 and result[i//3][0] + 1 < result[i][0]:
-        result[i][0] = result[i//3][0] + 1
-        result[i][1] = result[i//3][1] + [i]
-
-    if i % 2 == 0 and result[i//2][0] + 1 < result[i][0]:
-        result[i][0] = result[i//2][0] + 1
-        result[i][1] = result[i//2][1] + [i]
-        
-print(result[N][0])
-for i in result[N][1][::-1]: 
-    print(i, end=' ')
+while(q):
+    num,ans = q.popleft()
+    if num == 1:
+        print(len(ans)-1)
+        print(*ans)
+        break
+    if not visited[num]:
+        visited[num]=1
+        if num%3==0:
+            q.append((num//3,ans+[num//3]))
+        if num%2==0:
+            q.append((num//2,ans+[num//2]))
+        q.append((num-1,ans+[num-1]))
