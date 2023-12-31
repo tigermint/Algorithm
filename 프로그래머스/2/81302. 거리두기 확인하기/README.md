ff205fbabf48dee83a76dbed3650016d463a17cd
LEESEUNGYEOL <lsy12210@gmail.com>
2023-12-31 09:52:39 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/README.md
+++ 프로그래머스/2/81302. 거리두기 확인하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 거리두기 확인하기 - 81302 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.08 ms
+메모리: 10.3 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 0일 9:36:4
+2023년 12월 0일 9:52:37
 
 ### 문제 설명
 
--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -1,15 +1,16 @@
 def bfs(x,y,k):
     dx = [-1,0,0,1]
     dy = [0,-1,1,0]
-    for k1 in range(4):
+    
+    for k1 in range(4): # 상하좌우 check
         subx = x + dx[k1]
         suby = y + dy[k1]
-        if subx < 0 or subx >= 5 or suby < 0 or suby >= 5:
-            continue
-        if k[subx][suby] == 'P':
-            return False
-        if k[subx][suby] == 'O':
-            for k2 in range(4):
+        
+        if subx < 0 or subx >= 5 or suby < 0 or suby >= 5: continue # 끝자리인지 check
+        if k[subx][suby] == 'P': return False # 바로 옆자리 P면 false
+        
+        if k[subx][suby] == 'O': # 바로 옆자리 비어있을 경우
+            for k2 in range(4): # 맨해튼 거리 측정
                 subx2 = subx + dx[k2]
                 suby2 = suby + dy[k2]
                 if subx2 < 0 or subx2 >= 5 or suby2 < 0 or suby2 >= 5:
@@ -28,7 +29,7 @@ def solution(places):
         for i in range(5):
             for j in range(5):
                 if k[i][j] == 'P':
-                    if bfs(i,j,k) == False:
+                    if not bfs(i,j,k):
                         answer.append(0)
                         flag = 0
                         break
