87ea30add4519ea8811fb0e6990d55f53237e4d1
LEESEUNGYEOL <lsy12210@gmail.com>
2024-01-01 20:54:52 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -0,0 +1,24 @@
+from collections import deque
+dic = {
+    '}': '{',
+    ']': '[',
+    ')': '(',
+}
+def check(deq):
+    stack = []
+    for item in deq:
+        if not stack:
+            stack.append(item)
+        elif item not in dic:
+            stack.append(item)
+        else:
+            stack.pop() if dic[item] == stack[-1] else stack.append(item)
+    return not stack
+
+def solution(s):
+    answer = 0
+    deq = deque(s)
+    for _ in range(0, len(deq)):
+        deq.append(deq.popleft())
+        if check(deq): answer += 1 
+    return answer
