3ee35cd1d7cf4402d90cf6bbd7e15765809af3b6
LEESEUNGYEOL <lsy12210@gmail.com>
2024-01-16 11:46:50 +0900

    [level Lv.2] Title: [3차] n진수 게임, Time: 11.35 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/Lv.2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
+++ 프로그래머스/Lv.2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -1,11 +1,11 @@
 def convert(num, n):
-    arr = "0123456789ABCDEF"
+    digit = "0123456789ABCDEF"
     res = ""
     if num == 0:
         return '0'
     while num > 0:
         num, mod = divmod(num, n) # 계속 나누고, 나머지 index는 결과에 넣어주기
-        res += arr[mod]
+        res += digit[mod]
     return res[::-1]
    
 def solution(n, t, m, p):
