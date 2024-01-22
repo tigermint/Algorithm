36064c62c0883982ae5dd7c62bba290c4ae24f4d
LEESEUNGYEOL <lsy12210@gmail.com>
2024-01-22 12:04:48 +0900

    [level 0] Title: 문자열 섞기, Time: 9.34 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -2,7 +2,8 @@ class Solution {
     public String solution(String str1, String str2) {
         String answer = "";
         for(int i = 0; i < str1.length(); i++){
-            answer += str1.substring(i, i + 1) + str2.substring(i, i + 1);
+            answer+= str1.charAt(i);
+            answer+= str2.charAt(i);
         }
         return answer;
     }
