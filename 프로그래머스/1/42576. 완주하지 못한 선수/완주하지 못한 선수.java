fade8b9069ffa3bbb8714d61b6a66352196888da
LEESEUNGYEOL <lsy12210@gmail.com>
2024-04-10 12:51:01 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 81.38 ms, Memory: 95.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String, Integer> map = new HashMap<>();
+        for(String player : participant){
+            map.put(player, map.getOrDefault(player, 0) + 1);
+        }
+        
+        for(String player : completion){
+            map.put(player, map.get(player) - 1);
+        }
+        
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            if(entry.getValue() != 0){
+                answer = entry.getKey();
+                break;
+            }
+        }
+        return answer;
+    }
+}
